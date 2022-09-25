ec52c9503de218a87317b821f70811c28b9d09b3
lika <tagatalelika@gmail.com>
2022-09-26 08:40:41 +1100

    modif

---
+++ src/pages/Vuegenerale.vue
@@ -0,0 +1,18 @@
+<template>
+  <Layout>
+    <h1>Mon application Gridsome</h1>
+    <g-image alt="UNC NC" src="~/gridsome.png" width="135" />
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: 'About us'
+  }
+}
+</script>
+
+<style>
+
+</style>
